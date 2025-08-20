67364693a2dcf3ca7c170691f1640fc86b8e0cb5
Mandeep <23f2001951@ds.study.iitm.ac.in>
2025-08-20 15:48:38 +0530

    commiting exp 4

---
+++ Experiment-4/script.js
@@ -0,0 +1,14 @@
+function updateCount(textareaId, countId) {
+  var text = document.getElementById(textareaId).value;
+  document.getElementById(countId).innerText = text.length;
+}
+
+document.getElementById("text1").addEventListener("input", function() {
+  updateCount("text1", "count1");
+});
+
+document.getElementById("text2").addEventListener("input", function() {
+  updateCount("text2", "count2");
+});
+
+updateCount("text2", "count2");
